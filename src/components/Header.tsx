0d4c0198210513249294652540b4109bc46a0294
CarlosDR <dimatteoca@gmail.com>
2024-09-04 08:51:23 -0400

    change MenuLeftLink to MenuLink

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,16 @@
 import { FaHome } from 'react-icons/fa';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
-import MenuLeftLink from './MenuLeftLink';
 import { MdDarkMode } from 'react-icons/md';
 import Link from 'next/link';
+import MenuLink from './MenuLink';
 
 function Header() {
   return (
     <header className='flex justify-between  mx-auto h-[3rem] w-full p-3 max-w-6xl items-center'>
       <div className='menuLinksLeft flex gap-8 items-center '>
-        <MenuLeftLink route='/' title='home' Icon={FaHome}></MenuLeftLink>
-        <MenuLeftLink
-          route='/about'
-          title='about'
-          Icon={BsFillInfoCircleFill}
-        />
+        <MenuLink route='/' title='home' Icon={FaHome}></MenuLink>
+        <MenuLink route='/about' title='about' Icon={BsFillInfoCircleFill} />
       </div>
-
-
 
       <Link href='/' className='flex items-center gap-1 '>
         <span className='text-2xl font-bold bg-amber-500 text-white rounded-lg px-2 py-1'>
--- src/components/MenuLink.tsx
+++ src/components/MenuLink.tsx
@@ -2,24 +2,23 @@ import Link from 'next/link';
 import React from 'react';
 import { IconType } from 'react-icons';
 
-type MenuLeftLinkPropType = {
+type MenuLinkPropType = {
   route: string;
   title: string;
   Icon: IconType;
 };
 
-const MenuLeftLink = ({ route, title, Icon }: MenuLeftLinkPropType) => {
+const MenuLink = ({ route, title, Icon }: MenuLinkPropType) => {
   return (
     <>
       <Link href={route}>
         <p className='menuLink__title uppercase hidden font-bold text-[0.875rem] text-sm  sm:inline'>
           {title}
         </p>
-        <Icon className='menuLink__title sm:hidden text-2xl'>
-        </Icon>
+        <Icon className='menuLink__title sm:hidden text-2xl'></Icon>
       </Link>
     </>
   );
 };
 
-export default MenuLeftLink;
+export default MenuLink;
